8b4de57f7ebb5c1cdc2f6d7adb15c4dfc60b6855
lionelr <lionel.resnik@netapp.com>
2023-10-24 14:10:23 +0300

    overwrite file when downloading from bucket

--- src/main/java/com/chatbot/service/AmazonS3Service.java
+++ src/main/java/com/chatbot/service/AmazonS3Service.java
@@ -5,14 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 
 /**
@@ -47,15 +49,19 @@ public class AmazonS3Service {
 
     public boolean downloadFile(String bucketName, String key) {
         Path path = Paths.get(medicationsFilePath);
-        try {
-            s3Client.getObject(GetObjectRequest.builder().bucket(bucketName).key(key).build(),
-                               ResponseTransformer.toFile(path));
+        try (InputStream inputStream = s3Client.getObject(
+                GetObjectRequest.builder().bucket(bucketName).key(key).build())) {
+            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
             logger.info("File downloaded successfully from bucket {} with key {}", bucketName, key);
             return true;
         }
         catch (S3Exception e) {
             logger.error("Error occurred while trying to download file from bucket {} with key {}. Error: {}",
                          bucketName, key, e.getMessage());
+            return false;
+        }
+        catch (Exception e) {
+            logger.error("Error occurred while trying to write the downloaded file. Error: {}", e.getMessage());
             return false;
         }
     }
